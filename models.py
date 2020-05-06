26ee2929e589c298580f132486e72db06b19a0fd
root <qchalvondemersay@gmail.com>
2020-05-06 00:05:04 +0000

    add NN layers

--- models.py
+++ models.py
@@ -19,17 +19,57 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        ## output size = (W-F)/S +1 = (224-5)/1 +1 = 220
+        # the output Tensor for one image, will have the dimensions: (32, 220, 220)
+        # after one pool layer, this becomes (32, 110, 110)
         self.conv1 = nn.Conv2d(1, 32, 5)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
+        # maxpool layer
+        # pool with kernel_size=2, stride=2
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # second conv layer
+        ## output size = (W-F)/S +1 = (110-5)/1 +1 = 106
+        # the output Tensor for one image, will have the dimensions: (64, 106, 106)
+        # after one pool layer, this becomes (64, 53, 53)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        
+        # third conv layer
+        ## output size = (W-F)/S +1 = (53-5)/1 +1 = 49
+        # the output Tensor for one image, will have the dimensions: (128, 49, 49)
+        # after one pool layer, this becomes (128, 25, 25) # round down
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        
+        # 128 outputs * the 25 x 25 filtered / pooled map size
+        self.fc1 = nn.Linear(128*25*25, 1000)
+        
+        # dropout with p=0.5
+        self.fc1_drop = nn.Dropout(p=0.5)
+        
+        # create the last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        self.fc1 = nn.Linear(1000, 136)
 
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+        
+        # 3 conv/relu + pool layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        
+        # flattend for linear layer
+        x = x.view(x.size(0), -1)
+        
+        # two linear layers with dropout in between
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
         
         
         # a modified x, having gone through all the layers of your model, should be returned
